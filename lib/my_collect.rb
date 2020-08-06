4f534a5e6fa00fd80114389894da5be5f69f38b2
Kelin Field <kfield111@hotmail.com>
2020-08-06 00:39:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,9 @@
 def my_collect (collection)
   i = 0
+  new_array = []
   while i < collection.length
-    yield(collection[i])
+    new_array.push (yield(collection[i]))
     i += 1
+
   end
-collection
 end
