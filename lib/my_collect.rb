d629b9ad277fb33195fa534c612ecce99b7da6d4
Kelin Field <kfield111@hotmail.com>
2020-08-06 00:33:35 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 def my_collect (collection)
-  new_array = []
-  collection.each do |language|
-    new_array.push "#{language}"
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
   end
-  new_array
 end
